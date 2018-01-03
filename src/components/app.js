81b8748077cc05b63a96b559a077d5f3cb05d22f
ckim618 <ckim618@gmail.com>
2018-01-03 15:34:22 -0800

    added media query for title to make mobile responsive

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import GameContainer from './gameContainer';
 
 const App = () => (
     <div className="container">
+        <p className="title">South Park&nbsp;<span id="guessText">Guessing</span>&nbsp;<span id="gameText">Game</span></p>
         <GameContainer />
     </div>
 );
